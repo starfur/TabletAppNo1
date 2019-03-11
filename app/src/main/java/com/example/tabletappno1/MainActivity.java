5674b9f49382ca5d1a9f9d7ec14cebf505978045
FURKAN CÖMERT <furkan.comert@arcelik.com>
2019-03-11 13:24:42 +0300

    trial1

--- app/src/main/java/com/example/tabletappno1/MainActivity.java
+++ app/src/main/java/com/example/tabletappno1/MainActivity.java
@@ -2,7 +2,7 @@ package com.example.tabletappno1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+/*lets do some modification*/
 public class MainActivity extends AppCompatActivity {
 
     @Override
